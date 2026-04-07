a66fd4c0f6965342a313ee8020b267b3e1a04fca
Marc <148865740+MMP1010101@users.noreply.github.com>
2026-04-07 12:33:46 +0200

    Update ipconfigure.bash

--- ipconfigure.bash
+++ ipconfigure.bash
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "Configurando IP estática en enp1s0..."
+echo "Configurando IP estática en enp3s0..."
 
 sudo tee /etc/netplan/00-installer-config.yaml > /dev/null <<EOF
 # This is the network config written by 'subiquity'
 network:
   ethernets:
     enp1s0:
+      dhcp4: true
+    enp3s0:
       dhcp4: false
       addresses:
         - 172.31.74.220/24
@@ -16,4 +18,4 @@ EOF
 echo "Aplicando configuración..."
 sudo netplan apply
 
-echo "¡Listo! IP configurada: 172.31.74.220/24"
+echo "¡Listo! IP configurada en enp3s0: 172.31.74.220/24"
